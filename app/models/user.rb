7f1f64adfd1e10d312139cb7189998f5ba6db79f
hyugadayo <achano.0616@gmail.com>
2022-07-19 23:13:14 +0900

    エラーメッセージ表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+        validates :nickname, presence: true
+        VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze # 半角混合英数字
+        validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+        has_many :fishings
+        has_many :prefecture
 end
