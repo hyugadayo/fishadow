d7d83c26dbac3f26f7f5ba9129ecd041d6c6c0a0
hyugadayo <achano.0616@gmail.com>
2022-07-23 23:34:28 +0900

    fishings_controller createアクション定義

--- app/controllers/fishings_controller.rb
+++ app/controllers/fishings_controller.rb
@@ -8,5 +8,17 @@ class FishingsController < ApplicationController
     @fishings = Fishing.new
   end
 
+  def crate
+    @fishing = Fishing.new(fishing_params)
+    if @fishing.save
+      redirect_to fishings_path
+    else
+      render :new
+    end
+  end
 
+  private
+  def fishing_params
+    params.require(:fishing).permit(:get_date, :fish_name, :get_number, :size, :weight, :style, :explain, :wether, :lat, :lng, :image).merge(user_id: current_user.id)
+  end
 end
